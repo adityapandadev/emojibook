dba3b58b81d7e8cdc608a2b844ea894eef7d27bc
Aditya Panda <71640743+adityapandadev@users.noreply.github.com>
2021-05-14 20:24:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+// var sampleText = "Hello";
+
+// var color = "blue";
+
+// var userName = prompt("Enter Your Name...");
+// var likeCounter = 0;
+
+export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    var newLikeCounterValue = likeCounter + 1;
+    setLikeCounter(newLikeCounterValue);
+  }
+  // console.log("LikeCounter", likeCounter)
+  return (
+    <div className="App">
+      <h1>Emojibook</h1>
+      {/* <h2 style={{ color: color }}>Welcome {userName}</h2> */}
+      <button onClick={likeClickHandler}>Click me</button> {likeCounter}
+    </div>
+  );
+}
