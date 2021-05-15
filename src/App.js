0db6c13826931ef964ff07cac527e9de2c337436
Aditya Panda <71640743+adityapandadev@users.noreply.github.com>
2021-05-15 21:06:56 +0530

    added: input field

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-// var sampleText = "Hello";
-
-// var color = "blue";
-
-// var userName = prompt("Enter Your Name...");
-// var likeCounter = 0;
-
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
-
-  function likeClickHandler() {
-    var newLikeCounterValue = likeCounter + 1;
-    setLikeCounter(newLikeCounterValue);
+  var [userInput, setUserInput] = useState("");
+  function inputChangeHandler(event) {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
   }
-  // console.log("LikeCounter", likeCounter)
+
   return (
     <div className="App">
       <h1>Emojibook</h1>
-      {/* <h2 style={{ color: color }}>Welcome {userName}</h2> */}
-      <button onClick={likeClickHandler}>Click me</button> {likeCounter}
-
-     <p>done for now</p>
+      <input onChange={inputChangeHandler}></input>
+      <div> Welcome <span id="username"> {userInput} </span> </div>
     </div>
   );
 }
