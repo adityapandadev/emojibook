6c68dbb112652068b6b2a7c7bcf9e8f97078df28
Aditya Panda <71640743+adityapandadev@users.noreply.github.com>
2021-05-16 13:04:35 +0530

    upto emoji dictionary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictionary = {
+  "🤣": "laughing Hard",
+  "😊": "Blushing",
+  "🤔": "thinking face"
+};
+
 export default function App() {
-  var [userInput, setUserInput] = useState("");
-  function inputChangeHandler(event) {
-    console.log(event.target.value);
-    setUserInput(event.target.value);
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if(meaning === undefined){
+           meaning = "We don't have this in our database"
+    }
+
+    setMeaning(meaning);
   }
 
   return (
     <div className="App">
       <h1>Emojibook</h1>
-      <input onChange={inputChangeHandler}></input>
-      <div> Welcome <span id="username"> {userInput} </span> </div>
+      <input onChange={emojiInputHandler}></input>
+      <h3>{meaning}</h3>
     </div>
   );
 }
